f7561d21a189eb39de5851771133ee463f96e919
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-08-02 20:15:20 +0200

    Fix popup close

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import './style.css';
 
-const reserve = document.querySelector('.reserve-form');
-const comments = document.querySelector('.comments-form');
+const popup = document.querySelector('.popup-form');
 const commentsForm = document.querySelectorAll('.btn');
 const closeComment = document.querySelector('.close');
 commentsForm.forEach((comment) => {
   comment.addEventListener('click', (e) => {
     e.preventDefault();
-    if (e.target.classList.contains('opinion')) {
-      comments.style.display = 'block';
-      document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
-    } else if (e.target.classList.contains('reserve')) {
-      reserve.style.display = 'block';
-      document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
-    }
+    popup.style.display = 'block';
   });
 });
+
 closeComment.addEventListener('click', () => {
-    comments.style.display = 'none'; 
-    reserve.style.display = 'none';
-    
+  popup.style.display = 'none'; 
 });
