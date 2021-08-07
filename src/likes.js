6f0acabd1cc69ef65816bd5fa8791a256866280d
Collins Tatang <makungongcollins@gmail.com>
2021-08-07 10:42:33 +0100

    Fix linters

--- src/likes.js
+++ src/likes.js
@@ -1,8 +1,8 @@
-import { getLikes } from "./api"; 
+import getLikes  from './api.js';
 
 export const dspLikes = async (items) => {
   const getLike = await getLikes(items[0].id);
   getLike.forEach((element) => {
-    document.getElementById('l'+element.item_id).innerHTML += element.likes || 0;
+    document.getElementById('l' + element.item_id).innerHTML += element.likes || 0;
   });
 };
